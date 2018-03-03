552797cd2d2bbb044edc0dfe5a726bbc6aa29b56
Evgenii <evgenii@eulerlabs.ru>
2018-03-03 15:19:45 +0300

    Create tables.cpp

---
+++ tables.cpp
@@ -0,0 +1,152 @@
+
+#include <iostream>
+#include <vector>
+#include <math.h>
+#include <stack>
+#include <iomanip>
+#include <algorithm>
+
+using namespace std;
+
+struct Column {
+    Column () {}
+    Column (double nx, double ny) {
+        x = nx;
+        y = ny;
+    }
+    double x;
+    double y;
+};
+
+double dist(vector<Column> &columns, double v1, double v2) {
+    return sqrt((columns[v1].x - columns[v2].x)*(columns[v1].x - columns[v2].x) + (columns[v1].y - columns[v2].y)*(columns[v1].y - columns[v2].y));
+}
+
+bool dfs (vector<vector<double>> &graph, double maxD, int vertex, int rightWallIndex, vector<bool> &visited) {
+    stack <int> s;
+    s.push(vertex);
+
+    while(!s.empty()) {
+        int v = s.top();
+        visited[v] = true;
+        s.pop();
+
+        for (int i = 0; i < graph.size(); ++i) {
+            if (graph[v][i] < maxD && !visited[i]) {
+                if (i == rightWallIndex) {
+                    return true;
+                }
+                s.push(i);
+            }
+        }
+    }
+    return false;
+}
+
+
+bool isWay(vector<vector<double>> &graph, double maxD) {
+
+    vector <bool> visited(graph.size(), false);
+    vector <vector<int>> components;
+
+    int leftWallIndex = graph.size() - 2;
+    int rightWallIndex = graph.size() - 1;
+
+    return dfs(graph, maxD, leftWallIndex, rightWallIndex, visited);
+}
+
+double binSearch(vector<vector<double>> &graph, double maxD) {
+    double left = 0;
+    double right = maxD;
+    double mid = 0;
+
+//    vector<bool> visited(graph.size(), false);
+
+    while (right - left > 0.00000001) {
+        mid = (right + left) / 2;
+//        cout << "mid = " <<mid<<endl;
+
+        if (isWay(graph, mid)) {
+            right = mid;
+        } else {
+            left = mid;
+        }
+    }
+    return right;
+}
+
+int main() {
+
+    double xl = 0, xr = 0;
+    cin >> xl >> xr;
+    double r = 0;
+    cin >> r;
+    unsigned long n = 0;
+    cin >> n;
+
+    vector <Column> columns;
+
+    //Reading vertices
+    double newx = 0, newy = 0;
+    for (int i = 0; i < n; ++i) {
+        cin >> newx >> newy;
+        columns.emplace_back(Column(newx, newy));
+    }
+
+//    for (int i = 0; i < columns.size(); ++i) {
+//        cout << columns[i].x << " "<<columns[i].y << endl;
+//    }
+
+    // INITIALIZING GRAPH
+    vector<vector<double>> graph(n + 2);
+    for (int i = 0; i < n + 2; ++i) {
+        graph[i].resize(n + 2, 0);
+    }
+
+    for (int i = 0; i < n; ++i) {
+        for (int j = 0; j < n; ++j) {
+            if (i != j) {
+                double d = dist(columns, i, j) - 2 * r;
+                graph[i][j] = d;
+                graph[j][i] = d;
+
+//                cout << "Dist between " << i << " and " << j << " = "<<d<<endl;
+            }
+        }
+    }
+
+    double leftWallIndex = n;
+    double rightWallIndex = n + 1;
+
+    for (int i = 0; i < n; ++i) {
+        graph[leftWallIndex][i] = columns[i].x - xl - r;
+        graph[i][leftWallIndex] = columns[i].x - xl - r;
+//
+//        cout << "Dist between " << i << " and left wall = "<<columns[i].x<< " - "<< xl << " - " << r << " = "<< columns[i].x - xl - r<<endl;
+
+        graph[i][rightWallIndex] = xr - columns[i].x - r;
+        graph[rightWallIndex][i] = xr - columns[i].x - r;
+
+//        cout << "Dist between " << i << " and right wall = "<<xr<< " - "<< columns[i].x << " - " << r << " = "<< xr - columns[i].x - r<<endl;
+    }
+
+    graph[leftWallIndex][rightWallIndex] = xr - xl;
+    graph[rightWallIndex][leftWallIndex] = xr - xl;
+
+    cout.precision(3);
+    cout << fixed << round(binSearch(graph, xr - xl)) << endl;
+
+//    for (int i = 0; i < columns.size(); ++i) {
+//        cout << columns[i].x << " " << columns[i].y << endl;
+//    }
+//    cout << columns.size() << " " << n << endl;
+
+//    for (int i = 0; i < graph.size(); ++i) {
+//        for (int j = 0; j < graph.size(); ++j) {
+//            cout << graph[i][j] << " ";
+//        }
+//        cout << endl;
+//    }
+
+    return 0;
+}
